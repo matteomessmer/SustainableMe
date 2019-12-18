ad656205e1a60472f89444f65cd0ae268273151a
uniederstaetter <uniederstaetter@unibz.it>
2019-12-18 14:40:17 +0100

    imports

--- App.js
+++ App.js
@@ -3,6 +3,17 @@ import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import RegistrationScreen from 'Screens/RegistrationScreen.js';
+
+
+import HomeScreen from 'Screens/HomeScreen.js';
+import MissionLocationScreen from 'Screens/MissionLocationScreen.js';
+import MissionEnvironmentScreen from 'Screens/MissionEnvironmentScreen.js';
+import MissionRestaurantScreen from 'Screens/MissionRestaurantScreen.js';
+import MissionTransportScreen from 'Screens/MissionTransportScreen.js';
+import MissionCompletedScreen from 'Screens/MissionCompletedScreen.js';
+import MissionScreen from 'Screens/MissionScreen.js';
+import LoginScreen from 'Screens/LoginScreen.js';
 
 const routes = {
   Login: LoginScreen,
