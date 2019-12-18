71b5923ede5e44949f1bcea8b6a6a12fd4e1a3ce
chiaramistro <chiaramistro@gmail.com>
2019-12-18 14:40:59 +0100

    styles.js update

---
+++ styles.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+
+});
