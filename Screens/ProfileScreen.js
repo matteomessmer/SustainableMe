cb032c2040cbef7db51ed6bb899e61bceaa4e36e
uniederstaetter <uniederstaetter@unibz.it>
2020-01-06 12:21:25 +0100

    fixed indentations

--- Screens/ProfileScreen.js
+++ Screens/ProfileScreen.js
@@ -1,32 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import Profile from "../Components/Profile";
 import {Subscribe} from 'unstated';
 import ProfileContainer from '../Container/ProfileContainer';
 
 const ProfileScreen = props => {
-  return (
-    <Subscribe to={[ProfileContainer]}>
-			{
-				profileContainer => (
-        <Profile
-          user={profileContainer.state.user}
-          editPassword={(id, oldPassword, newPassword)=>profileContainer.editPassword(id, oldPassword, newPassword)}
-          editUser={(user)=>profileContainer.editUser(user)}
-          logout={()=>props.navigation.navigate('Login')}
-        />
-        )
-      }
-    </Subscribe>
-  )
+    return (
+        <Subscribe to={[ProfileContainer]}>
+            {profileContainer => (
+                <Profile
+                    user={profileContainer.state.user}
+                    editPassword={(id, oldPassword, newPassword) => profileContainer.editPassword(id, oldPassword, newPassword)}
+                    editUser={(user) => profileContainer.editUser(user)}
+                    logout={() => props.navigation.navigate('Login')}
+                />
+            )
+            }
+        </Subscribe>
+    )
 };
 
 ProfileScreen.navigationOptions = {
-  title: 'Profile',
-  headerTintColor: '#ffffff',
-  headerStyle: {
-    backgroundColor: '#417110'
-  }
+    title: 'Profile',
+    headerTintColor: '#ffffff',
+    headerStyle: {
+        backgroundColor: '#417110'
+    }
 };
 
 export default ProfileScreen;
