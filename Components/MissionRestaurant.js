639b11d6f7a2e61418b67603871268013cbb6599
uniederstaetter <uniederstaetter@unibz.it>
2020-01-10 13:20:11 +0100

    added some todos

--- Components/MissionRestaurant.js
+++ Components/MissionRestaurant.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import { Text, View,ScrollView, Button, Image } from 'react-native';
-import { styles} from '../styles.js';
+import {Text, View, ScrollView, Button, Image} from 'react-native';
+import {styles} from '../styles.js';
 
-export default class MissionRestaurant extends React.Component{
+export default class MissionRestaurant extends React.Component {
 
     constructor(props) {
         super(props);
     }
 
-	render(){
-		return (
-			<View>
-				<Image
-					style={{width: 400, height: 200}}
-					source={{uri: this.props.mission.image}}
-					/>
-				<Text>{this.props.mission.name}</Text>
-				<Text>{this.props.mission.address}</Text>
-				<Text>{this.props.mission.points}</Text>
-				<Text>{this.props.mission.qr}</Text>
-				<Text>{this.props.mission.url}</Text>
-				<Text>{this.props.mission.description}</Text>
-			</View>
-		)
-	}
+    render() {
+        return (
+            <View>
+                <Image
+                    style={{width: 400, height: 200}}
+                    source={{uri: this.props.mission.image}}
+                />
+                <Text>{this.props.mission.name}</Text>
+                <Text>{this.props.mission.address}</Text>
+                <Text>{this.props.mission.points}</Text>
+                <Text>{this.props.mission.qr}</Text>
+                <Text>{this.props.mission.url}</Text>
+                <Text>{this.props.mission.description}</Text>
+            </View>
+        )
+    }
 
 }
--- Container/RestaurantContainer.js
+++ Container/RestaurantContainer.js
@@ -2,30 +2,30 @@ import React from 'react';
 import {Container} from 'unstated'
 
 export default class RestaurantContainer extends Container {
-    state = {
-    }
-	
-	getRestaurants = async () => {
-  		const response = await fetch('http://sustainableme.fablabnetwork.tk/API/getMissions.php', {
-  			method: 'POST',
-  			headers: {
-  				Accept: 'application/json',
-  				'Content-Type': 'application/json',
-  			},
-  			body: null,
-  		}).catch((error) => {
-  			console.error(error);
-  			return null;
-  		});
-		
-		const responseJson = await response.json();
-		const result = await responseJson.missionsRestaurant;
+    state = {}
 
-  		if(responseJson.error) {
-  			alert(responseJson.description);
-  			return null;
-  		} else {
-  			return result;
-  		}
-	}
+    //TODO: why post request with empty body? Why not having get?
+    getRestaurants = async () => {
+        const response = await fetch('http://sustainableme.fablabnetwork.tk/API/getMissions.php', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: null,
+        }).catch((error) => {
+            console.error(error);
+            return null;
+        });
+
+        const responseJson = await response.json();
+        const result = await responseJson.missionsRestaurant;
+
+        if (responseJson.error) {
+            alert(responseJson.description);
+            return null;
+        } else {
+            return result;
+        }
+    }
 }
