4ad3f0a11dd6604ee64526c8dc06222839d4b214
chiaramistro <chiaramistro@gmail.com>
2020-01-11 18:56:48 +0100

    fixed resetting path after mission completed

--- Screens/MissionCompletedScreen.js
+++ Screens/MissionCompletedScreen.js
@@ -4,6 +4,7 @@ import {Subscribe} from "unstated";
 import ProfileContainer from "../Container/ProfileContainer";
 import UserContainer from "../Container/UserContainer";
 import PointsContainer from "../Container/PointsContainer";
+import { NavigationActions, StackActions } from 'react-navigation'
 
 //Screen to show the information about the mission that the user just completed
 const MissionCompletedScreen = props => {
@@ -14,15 +15,31 @@ const MissionCompletedScreen = props => {
         <Subscribe to={[ProfileContainer, UserContainer, PointsContainer]}>
             {(profileContainer, userContainer, pointscontainer) => (
 
-                <MissionCompleted
-                    onOther={() => props.navigation.navigate("Mission")}
-                    mission={nameOfMission}
-                    totalPoints={pointscontainer.state.totalPoints}
-                    pointsLeft={total=>userContainer.computePointsLeft(total)}
-                />
-            )
-            }
-        </Subscribe>
+              <MissionCompleted
+  onOther={ () => {
+   props
+     .navigation
+     .dispatch(StackActions.reset({
+       index: 0,
+       actions: [
+NavigationActions.navigate({
+routeName: 'Home', // Call home stack
+action: NavigationActions.navigate({
+routeName: 'Mission', // Navigate to this screen
+}),
+}),
+],
+       key: null
+     }))
+  }
+}
+  mission={nameOfMission}
+  totalPoints={pointscontainer.state.totalPoints}
+  pointsLeft={total=>userContainer.computePointsLeft(total)}
+/>
+)
+}
+</Subscribe>
     )
 
 };
